09c0ca43c6ee0d79c666ee465224ed9ee1354eca
leftthomas <leftthomas@qq.com>
2018-05-02 14:54:06 +0800

    change model

--- model.py
+++ model.py
@@ -2,12 +2,12 @@ from torch import nn
 
 
 class Model(nn.Module):
-    def __init__(self, hidden_dim, emb_dim=300, recurrent_dropout=0.1):
+    def __init__(self):
         super(Model, self).__init__()
-        self.embedding = nn.Embedding(784, emb_dim)
-        self.encoder = nn.LSTM(emb_dim, hidden_dim, num_layers=1, dropout=recurrent_dropout)
-        self.linear_layers = nn.Linear(hidden_dim, hidden_dim)
-        self.predictor = nn.Linear(hidden_dim, 6)
+        self.embedding = nn.Embedding(784, 100)
+        self.encoder = nn.LSTM(100, 500, num_layers=1, dropout=0.1)
+        self.linear_layers = nn.Linear(500, 500)
+        self.predictor = nn.Linear(500, 6)
 
     def forward(self, seq):
         hdn, _ = self.encoder(self.embedding(seq))
