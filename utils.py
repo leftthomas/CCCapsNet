865b43c06867e3b4b0ab8587f26ca398f74c8162
leftthomas <leftthomas@qq.com>
2018-05-02 16:06:05 +0800

    fix bug of data load

--- utils.py
+++ utils.py
@@ -3,7 +3,7 @@ from torchtext.datasets import TREC
 
 def get_iterator(data_type, mode, batch_size=100):
     if data_type == 'TREC':
-        train_data, test_data = TREC.iters(batch_size=batch_size, device=-1, root='data')[1]
+        train_data, test_data = TREC.iters(batch_size=batch_size, device=-1, root='data')
         return train_data if mode else test_data
     else:
         raise NotImplementedError()
