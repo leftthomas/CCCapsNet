3fabfd864357fb54a10ca008059f7f6235755a15
leftthomas <leftthomas@qq.com>
2018-05-11 15:40:23 +0800

    add max_size=15000 to build vocab

--- utils.py
+++ utils.py
@@ -41,7 +41,7 @@ def load_data(data_type, batch_size, fine_grained):
         # IMDB
         train, test = IMDB.splits(text, label, root='data')
 
-    text.build_vocab(train, vectors='glove.6B.300d')
+    text.build_vocab(train, vectors='glove.6B.300d', max_size=15000)
     label.build_vocab(train)
 
     train_iter, test_iter = data.BucketIterator.splits((train, test), batch_size=batch_size, repeat=False, device=-1)
