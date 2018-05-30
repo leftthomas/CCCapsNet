8d44f36da1f303b90fc6881e4665b3b035aff3c8
leftthomas <leftthomas@qq.com>
2018-05-30 19:33:58 +0800

    finish amazon dataset

--- datasets/amazon.py
+++ datasets/amazon.py
@@ -1,60 +1,75 @@
 import os
 
+import numpy as np
+import pandas as pd
 from torchnlp.datasets.dataset import Dataset
 from torchnlp.download import download_file_maybe_extract
 
 
-def amazon_dataset(directory='data/', train=False, test=False, check_files=['aclImdb/README'],
+def amazon_dataset(directory='data/', train=False, test=False, check_files=['readme.txt'],
                    # amazon_review_full, amazon_review_polarity
                    urls=['https://drive.google.com/uc?export=download&id=1ps9Sh8i9i-1JId-37InvUbk0t4stxh6L',
                          'https://drive.google.com/uc?export=download&id=1Qv9vdBf2LfbQsfcKfMsHAEuSsPCWYUk_'],
                    fine_grained=False):
     """
-    Load the AG's News Topic Classification dataset (Version 3).
+    Load the Amazon Review Full Score or Amazon Review Polaridy dataset (Version 3).
 
-    The AG's news topic classification dataset is constructed by choosing 4 largest classes
-    from the original corpus. Each class contains 30,000 training samples and 1,900 testing
-    samples. The total number of training samples is 120,000 and testing 7,600.
+    The Amazon reviews full score dataset is constructed by randomly taking 600,000 training samples and
+    130,000 testing samples for each review score from 1 to 5. In total there are 3,000,000 trainig samples
+    and 650,000 testing samples.
+    The Amazon reviews polarity dataset is constructed by taking review score 1 and 2 as negative, and 4 and
+    5 as positive. For each polarity 1,800,000 training samples and 200,000 testing samples are take randomly.
+    In total there are 3,600,000 trainig samples and 400,000 testing samples. Negative polarity is class 1,
+    and positive class 2.
 
-    **Reference:** http://www.di.unipi.it/~gulli/AG_corpus_of_news_articles.html
+    **Reference:** http://jmcauley.ucsd.edu/data/amazon/
 
     Args:
         directory (str, optional): Directory to cache the dataset.
         train (bool, optional): If to load the training split of the dataset.
         test (bool, optional): If to load the test split of the dataset.
-        extracted_name (str, optional): Name of the extracted dataset directory.
         check_files (str, optional): Check if these files exist, then this download was successful.
-        urls (str, optional): URL of the dataset `tar.gz` file.
+        urls (str, optional): URLs to download.
+        fine_grained (bool, optional): Whether to use 5-class instead of 2-class labeling. Which means using
+        amazon_review_full dataset instead of amazon_review_polarity dataset
 
     Returns:
         :class:`tuple` of :class:`torchnlp.datasets.Dataset`: Tuple with the training dataset and
         test dataset in order if their respective boolean argument is true.
 
     Example:
-        >>> from datasets import ag_dataset
-        >>> train = ag_dataset(train=True)
+        >>> from datasets import amazon_dataset
+        >>> train = amazon_dataset(train=True)
         >>> train[0:2]
         [{
-          'label': '3',
-          'title': 'Wall St. Bears Claw Back Into the Black (Reuters)',
-          'description': "Reuters - Short-sellers, Wall Street's dwindling..."},
+          'label': '2',
+          'title': 'Stuning even for the non-gamer',
+          'text': 'This sound track was beautiful! It paints the senery in your mind so...'}
          {
-          'label': '3',
-          'title': 'Carlyle Looks Toward Commercial Aerospace (Reuters)',
-          'description': 'Reuters - Private investment firm Carlyle Group...'}]
+          'label': '2',
+          'title': 'The best soundtrack ever to anything.',
+          'text': "I'm reading a lot of reviews saying that this is the best 'game soundtrack'..."}]
     """
-    download_file_maybe_extract(url=url, directory=directory, check_files=check_files)
+    if fine_grained:
+        extracted_name, url = 'amazon_review_full', urls[0]
+    else:
+        extracted_name, url = 'amazon_review_polarity', urls[1]
+    for i in range(len(check_files)):
+        check_files[i] = os.path.join(extracted_name, check_files[i])
+    download_file_maybe_extract(url=url, directory=directory, filename=extracted_name + '.tar.gz',
+                                check_files=check_files)
 
     ret = []
     splits = [file_name for (requested, file_name) in [(train, 'train.csv'), (test, 'test.csv')] if requested]
     for file_name in splits:
-        csv_file = csv.reader(open(os.path.join(directory, extracted_name, file_name), 'r', encoding='utf-8'))
+        csv_file = np.array(pd.read_csv(os.path.join(directory, extracted_name, file_name), header=None)).tolist()
         examples = []
         for data in csv_file:
-            examples.append({'label': data[0], 'title': data[1], 'description': data[2]})
+            examples.append({'label': str(data[0]), 'title': data[1], 'text': data[2]})
         ret.append(Dataset(examples))
 
     if len(ret) == 1:
         return ret[0]
     else:
         return tuple(ret)
+
