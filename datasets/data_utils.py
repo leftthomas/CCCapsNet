22b3ae4e306b9b011176b69e11fdfd875576e1a8
leftthomas <leftthomas@qq.com>
2018-06-03 16:51:30 +0800

    add stopwords

--- datasets/data_utils.py
+++ datasets/data_utils.py
@@ -1,4 +1,11 @@
+import os
 import re
+
+stopwords = []
+with open(os.path.join('data', 'stopwords.txt'), 'r', encoding='utf-8') as foo:
+    for line in foo.readlines():
+        line = line.rstrip('\n')
+        stopwords.append(line)
 
 
 def text_preprocess(text):
@@ -10,4 +17,11 @@ def text_preprocess(text):
     text = text.lower()
     # Substitute multiple SPACES by a single SPACE.
     text = ' '.join(text.split())
+    # Remove words that are less than 3 characters long. For example, removing "he" but keeping "him"
+    text = ' '.join(word for word in text.split() if len(word) >= 3)
+    # Remove the 524 SMART stopwords (the original stop word list contains 571 words, but there are 47 words contain
+    # hyphens, so we removed them, and we found the word 'would' appears twice, so we also removed it, the final stop
+    # word list contains 523 words). Some of them had already been removed, because they were shorter than 3 characters.
+    # the original stop word list can be found from http://www.lextek.com/manuals/onix/stopwords2.html
+    text = ' '.join(word for word in text.split() if word not in stopwords)
     return text
