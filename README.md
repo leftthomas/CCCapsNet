a89cb7c39d6910f25a5c22085fde38f68778c945
leftthomas <leftthomas@qq.com>
2019-05-19 12:29:28 +0800

    add vis embedding

--- README.md
+++ README.md
@@ -69,7 +69,7 @@ optional arguments:
 --in_length              in capsule length, it only works for capsule classifier [default value is 8]
 --out_length             out capsule length, it only works for capsule classifier [default value is 16]
 --num_iterations         routing iterations number, it only works for capsule classifier [default value is 3]
---batch_size             train batch size [default value is 60]
+--batch_size             train batch size [default value is 30]
 --num_epochs             train epochs number [default value is 10]
 --num_steps              test steps number [default value is 100]
 --load_model_weight      saved model weight to load [default value is None]
---
+++ vis.py
@@ -0,0 +1,38 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn import manifold, datasets
+
+digits = datasets.load_digits(n_class=6)
+X, y = digits.data, digits.target
+n_samples, n_features = X.shape
+
+'''显示原始数据'''
+n = 20  # 每行20个数字，每列20个数字
+img = np.zeros((10 * n, 10 * n))
+for i in range(n):
+    ix = 10 * i + 1
+    for j in range(n):
+        iy = 10 * j + 1
+        img[ix:ix + 8, iy:iy + 8] = X[i * n + j].reshape((8, 8))
+plt.figure(figsize=(8, 8))
+plt.imshow(img, cmap=plt.cm.binary)
+plt.xticks([])
+plt.yticks([])
+plt.show()
+
+'''t-SNE'''
+tsne = manifold.TSNE(n_components=2, init='pca', random_state=501)
+X_tsne = tsne.fit_transform(X)
+
+print("Org data dimension is {}. Embedded data dimension is {}".format(X.shape[-1], X_tsne.shape[-1]))
+
+'''嵌入空间可视化'''
+x_min, x_max = X_tsne.min(0), X_tsne.max(0)
+X_norm = (X_tsne - x_min) / (x_max - x_min)  # 归一化
+plt.figure(figsize=(8, 8))
+for i in range(X_norm.shape[0]):
+    plt.text(X_norm[i, 0], X_norm[i, 1], str(y[i]), color=plt.cm.Set1(y[i]),
+             fontdict={'weight': 'bold', 'size': 9})
+plt.xticks([])
+plt.yticks([])
+plt.show()
